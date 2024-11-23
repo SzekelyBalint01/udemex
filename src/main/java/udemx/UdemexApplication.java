5e58c335c4c4cc200c4e2ffb316c54a7fa44abe3
balintszekely <szekelybalint0101@gmail.com>
2024-11-23 11:44:35 +0100

    Initial commit

---
+++ src/main/java/udemx/UdemexApplication.java
@@ -0,0 +1,13 @@
+package udemx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/udemx/UdemexApplicationTests.java
@@ -0,0 +1,13 @@
+package udemx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
