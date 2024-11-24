893e7a7ea687fdf23fe1a3c061f9cf8138535dc8
balintszekely <szekelybalint0101@gmail.com>
2024-11-24 12:42:04 +0100

    Database Update with users and reservations tables

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,6 +1,24 @@
-CREATE TABLE autok (
+CREATE TABLE cars (
                        id SERIAL PRIMARY KEY,
                        neve VARCHAR(255) NOT NULL,
                        foto BYTEA,
                        aktiv BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE reservations(
+        id SERIAL PRIMARY KEY,
+        carId INTEGER,
+        startDate DATE,
+        endDate Date,
+);
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    address VARCHAR(255),
+    phone VARCHAR(17),
+    rentDays INTEGER,
+    price INTEGER,
+    carId INTEGER
 );
