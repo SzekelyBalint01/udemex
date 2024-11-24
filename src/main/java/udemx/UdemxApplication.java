fb4586cf89a067a6857174c556d7430b07b98cab
balintszekely <szekelybalint0101@gmail.com>
2024-11-24 21:13:39 +0100

    CloudWatcher logging implement

--- src/main/java/udemx/UdemxApplication.java
+++ src/main/java/udemx/UdemxApplication.java
@@ -1,13 +1,30 @@
 package udemx;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UdemxApplication {
+    private static final Logger logger = LoggerFactory.getLogger(UdemxApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(UdemxApplication.class, args);
+
+        // Logolás az alkalmazás indulása előtt
+        logger.info("Az alkalmazás indul...");
+
+        try {
+            // Spring Boot alkalmazás indítása
+            SpringApplication.run(UdemxApplication.class, args);
+
+            // Logolás az alkalmazás sikeres indulása után
+            logger.info("Az alkalmazás sikeresen elindult!");
+        } catch (Exception e) {
+            // Hiba logolása, ha valami nem sikerült
+            logger.error("Hiba történt az alkalmazás indítása során.", e);
+        }
     }
+
 
 }
