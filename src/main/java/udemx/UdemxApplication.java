a162ddb9051b2ae24ecbcfaa104b79c907e4bafd
balintszekely <szekelybalint0101@gmail.com>
2024-11-25 14:13:31 +0100

    Cloudwatch Logging

--- src/main/java/udemx/UdemxApplication.java
+++ src/main/java/udemx/UdemxApplication.java
@@ -1,12 +1,9 @@
 package udemx;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UdemxApplication {
-    //private static final Logger logger = LoggerFactory.getLogger(UdemxApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(UdemxApplication.class, args);
