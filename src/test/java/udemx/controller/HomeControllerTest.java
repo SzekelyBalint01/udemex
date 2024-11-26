9dc275df069155c4f83196ec6262af4ddca0feca
balintszekely <szekelybalint0101@gmail.com>
2024-11-26 18:17:12 +0100

    Implement tests

--- src/test/java/udemx/controller/HomeControllerTest.java
+++ src/test/java/udemx/controller/HomeControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HomeController.class)
+@SpringBootTest
 public class HomeControllerTest {
 
     @Autowired
