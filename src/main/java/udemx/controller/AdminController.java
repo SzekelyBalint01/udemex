0c187b3c30d8d19483f00dc5309386c3a34eda30
balintszekely <szekelybalint0101@gmail.com>
2024-11-28 15:51:34 +0100

    DB cerds fix

--- src/main/java/udemx/controller/AdminController.java
+++ src/main/java/udemx/controller/AdminController.java
@@ -1,17 +1,28 @@
 package udemx.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import udemx.model.Reservation;
+import udemx.pojo.CarDto;
+import udemx.pojo.CarResponseDto;
+import udemx.pojo.ReservationDto;
 import udemx.service.CarService;
 import udemx.service.ReservationService;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 
 @Controller
 public class AdminController {
 
     private final CarService carService;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private final ReservationService reservationService;
 
@@ -23,8 +34,18 @@ public class AdminController {
     @GetMapping("/admin")
     public String admin(Model model) {
 
-        model.addAttribute("reservations",  reservationService.getAllReservations());
-        model.addAttribute("cars", carService.getAllCars());
+        List<ReservationDto> reservations = Optional.ofNullable(reservationService.getAllReservations())
+                .orElse(Collections.emptyList());
+
+        logger.debug("Reservations: {}", reservations);
+
+        List<CarResponseDto> cars = Optional.ofNullable(carService.getAllCars())
+                .orElse(Collections.emptyList());
+
+        logger.debug("Cars: {}", cars);
+
+        model.addAttribute("reservations", reservations);
+        model.addAttribute("cars", cars);
 
         return "adminPage";
     }
